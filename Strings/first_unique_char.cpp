637c02680a38e3ce873b997aa00605260817533e
Naman2626 <your-email@example.com>
2025-07-16 18:27:34 +0530

    Day 2

---
+++ Strings/first_unique_char.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string str = "naan";
+     
+    for(int i = 0; i < str.length(); i++){
+        int count = 0 ;
+        for(int j = 0; j < str.length(); j++){
+
+            if(str[i] == str[j]) count++;
+        }
+
+        if(count == 1) {
+            cout << i;
+        }
+
+
+        }
+     
+}
